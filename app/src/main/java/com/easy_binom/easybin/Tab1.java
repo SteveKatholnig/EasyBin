62746934edf8171ed3fcf9fa47c57d69d97ddc1b
SteveKatholnig <Steve.Katholnig@googlemail.com>
2015-10-07 15:41:32 +0200

    updated comments

--- app/src/main/java/com/easy_binom/easybin/Tab1.java
+++ app/src/main/java/com/easy_binom/easybin/Tab1.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-//test
 public class Tab1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
